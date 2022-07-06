0056c2e86ff8e312c7df6182c56f6310ade72a1c
Victor Mendes <victorutdsim7@gmail.com>
2022-07-06 12:32:42 -0300

    requisito 5

--- script.js
+++ script.js
@@ -6,15 +6,36 @@ function createCircle() {
     circlesContainer.appendChild(circle);
   }
 }
+
 createCircle();
 
 function generateRandomColor() {
-  return `rgb(${Math.random() * 255}, ${Math.random() * 255}, ${Math.random() * 255})`;
+  const randomNumber1 = Math.floor(Math.random() * 255);
+  const randomNumber2 = Math.floor(Math.random() * 255);
+  const randomNumber3 = Math.floor(Math.random() * 255);
+  return `rgb(${randomNumber1}, ${randomNumber2}, ${randomNumber3})`;
 }
 
-window.onload = () => {
-  const circles = document.getElementsByClassName('ball');
-  for (let index = 0; index < circles.length; index += 1) {
+const colorToGuess = document.getElementById('rgb-color');
+const specificColor = generateRandomColor();
+colorToGuess.innerText = specificColor;
+
+const circles = document.getElementsByClassName('ball');
+const randomCircle = circles[Math.floor(Math.random() * circles.length)];
+randomCircle.style.backgroundColor = specificColor;
+console.log(randomCircle);
+for (let index = 0; index < circles.length; index += 1) {
+  if (circles[index].style.backgroundColor !== specificColor) {
     circles[index].style.backgroundColor = generateRandomColor();
   }
-};
+}
+const answer = document.getElementById('answer');
+for (let index = 0; index < circles.length; index += 1) {
+  circles[index].addEventListener('click', (event) => {
+    if (event.target.style.backgroundColor === specificColor) {
+      answer.innerText = 'Acertou!';
+    } else {
+      answer.innerText = 'Errou! Tente novamente!';
+    }
+  });
+}
